5e36e6abe45188343d470c28a612d63c539472e1
LuisaJanineAgas <luisajanineagas12@gmail.com>
2026-06-04 00:04:34 +0800

    Run testing Fan 1 and fixing the display

--- creating_fan_class.py
+++ creating_fan_class.py
@@ -3,7 +3,7 @@ class Fan:
     # class constants
     SLOW = 1
     MEDIUM = 2
-    HARD = 3
+    FAST = 3
 
     def __init__(self, speed=SLOW, on=False, radius=5, color="blue"):
         # private member
@@ -27,7 +27,7 @@ class Fan:
 
     # Mutators (Setters)
     def set_speed(self, speed):
-        if speed in [Fan.SLOW, Fan.MEDIUM, Fan.HARD]:
+        if speed in [Fan.SLOW, Fan.MEDIUM, Fan.FAST]:
             self.__speed = speed
         else:
             print("Invalid speed value")
@@ -44,3 +44,34 @@ class Fan:
     def set_color(self, color):
         self.__color = color
 
+    def __str__(self):
+        # Lines of info
+        lines = [
+            f"speed={self.__speed}",
+            f"on={self.__on}",
+            f"radius={self.__radius}",
+            f"color={self.__color}"
+        ]
+        # Find the longest line length
+        width = max(len(line) for line in lines)
+        # Top border
+        box = "\n+" + "-" * (width + 2) + "+\n"
+        # Content lines centered inside the box
+        for line in lines:
+            box += "| " + line.ljust(width) + " |\n"
+        # Bottom border
+        box += "+" + "-" * (width + 2) + "+"
+        return box
+
+# Test program
+def test_fan():
+    # First Fan Object
+    fan1 = Fan()
+    fan1.set_speed(Fan.FAST)
+    fan1.set_radius(10)
+    fan1.set_color("yellow")
+    fan1.set_on(True)
+
+    print("Fan 1: ", fan1)
+
+test_fan()
