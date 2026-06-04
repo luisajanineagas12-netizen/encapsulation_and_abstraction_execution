e8398cc6606ec77e957c8b5c66449976e2e6fec8
LuisaJanineAgas <luisajanineagas12@gmail.com>
2026-06-04 10:27:22 +0800

    fix the mutators since i fotgot the method parameter

--- class_pet/creating_pet_class.py
+++ class_pet/creating_pet_class.py
@@ -4,14 +4,14 @@ class Pet:
         self.__animal_type = animal_type
         self.__age = age
 
-    def set_name(self):
-        self.__name = self.__name
+    def set_name(self, name):
+        self.__name = name
 
-    def set_animal_type(self):
-        self.__animal_type = self.__animal_type
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
 
-    def set_age(self):
-        self.__age = self.__age
+    def set_age(self, age):
+        self.__age = age
 
     def get_name(self):
         return self.__name
@@ -21,4 +21,10 @@ class Pet:
 
     def get_age(self):
         return self.__age
+
+def pet():
     
+
+
+
+
